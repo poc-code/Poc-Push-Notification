a317bdef9ebdc73c9bc7bcc75de66a1661078387
uirapeixoto <uira.peixoto@gmail.com>
2020-12-01 14:12:56 -0300

    feat: jwt configuration

---
+++ src/poc-push-notification.domain/Model/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace poc_push_notification.domain.Model
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
---
+++ src/poc-push-notification.domain/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace poc_push_notification.domain.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+
+        [JsonIgnore]
+        public string Password { get; set; }
+    }
+}
